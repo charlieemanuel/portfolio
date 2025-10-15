f316a5bd7f9da8d7719c93a02233841c812ad250
unknown <charlieemanuel8@gmail.com>
2025-10-15 17:34:24 +0800

    finalizing about section margins

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -13,17 +13,17 @@ const About = () => {
                 I’m a UI/UX Designer and Web Developer who loves turning ideas into interactive, visually engaging websites. With a strong eye for design and a passion for user experience, I blend creativity and logic to craft digital products that feel as good as they look. Whether it’s designing intuitive interfaces or coding responsive layouts, I’m dedicated to delivering high-quality work that meets both user needs and business goals. Let’s create something amazing together!
             </p>
         </div>
-        <div className="flex flex-col md:flex-row max-md:space-y-5 justify-center items-center mt-8">
+        <div className="flex flex-col md:flex-row max-md:space-y-12 justify-center items-center mt-8">
             <div className="flex flex-col justify-center items-center text-xl text-white font-bold p-4 rounded-xl max-w-[720px] h-[420px]">
-                <img src={Design} alt="Picture" className='rounded-xl w-full h-full mb-2' />
+                <img src={Design} alt="Picture" className='rounded-xl w-full h-full mb-4' />
                 <p>Design!</p>
             </div>
             <div className="flex flex-col justify-center items-center text-xl text-white font-bold p-4 rounded-xl max-w-[720px] h-[420px]">
-                <img src={Code} alt="Picture"  className='rounded-xl w-full h-full mb-2' />
+                <img src={Code} alt="Picture"  className='rounded-xl w-full h-full mb-4' />
                 <p>Code!</p>
             </div >
             <div className="flex flex-col justify-center items-center text-xl text-white font-bold p-4 rounded-xl max-w-[720px] h-[420px]">
-                <img src={Chill} alt="Picture"  className='rounded-xl w-full h-full mb-2' />
+                <img src={Chill} alt="Picture"  className='rounded-xl w-full h-full mb-4' />
                 <p>Chill!</p>
             </div >
         </div>
