655db694e2dd05a70a021eddb4f0c5c063541caa
Charlie Emanuel Daga-as <charlieemanuel8@gmail.com>
2025-10-16 10:05:43 +0800

    minimal adjustment in skills section

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -27,7 +27,7 @@ const Skills = () => {
     <section className="flex flex-col flex-wrap p-8" id="skills">
         <div className='flex flex-col flex-wrap'>
             <h2 className="text-2xl sm:text-3xl font-bold tracking-tight text-[#6e6e6e] hero-shadow mt-7 mb-2">My Toolkit</h2>
-            <p className="text-2xl text-gray-400 font-bold">From sleek front-end builds to dynamic animations, I use modern tools like:</p>
+            <p className="text-xl text-gray-400 font-bold">From sleek front-end builds to dynamic animations, I use modern tools like:</p>
         </div>
         <div className='flex justify-center items-center space-x-30 mt-20 logo-wrapper'>
             <div className='flex flex-row justify-center items-center space-x-15 md:space-x-25 logo-list'>
@@ -88,7 +88,7 @@ const Skills = () => {
             </div>
         </div>
         <div>
-            <p className="text-2xl text-gray-400 font-bold mt-20">Every project is built for performance, beauty, and scalability.</p>
+            <p className="text-xl text-gray-400 font-bold mt-20">Every project is built for performance, beauty, and scalability.</p>
         </div>
     </section>
   )
