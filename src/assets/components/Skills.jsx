184e6f413788ac55b3ace2143d994ec96cc3f6ef
Charlie Emanuel Daga-as <charlieemanuel8@gmail.com>
2025-10-16 09:31:58 +0800

    finalizing skills section

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -1,3 +1,5 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import JSlogo from '../logos/javascript-1.svg';
 import HTMLlogo from '../logos/html-1.svg';
 import CSSlogo from '../logos/css-3.svg';
@@ -9,22 +11,81 @@ import GitLogo from '../logos/git-icon.svg';
 import FigmaLogo from '../logos/figma-icon.svg';
 
 const Skills = () => {
+    useGSAP(() => {
+
+        gsap.fromTo('.logo-list', {
+            xPercent: -35,
+        }, {
+            xPercent: 45,
+            duration: 15,
+            repeat: -1,
+            yoyo: true,
+            ease: "power",
+        })
+    })
   return (
     <section className="flex flex-col flex-wrap p-8" id="skills">
-        <div>
+        <div className='flex flex-col flex-wrap'>
             <h2 className="text-2xl sm:text-3xl font-bold tracking-tight text-[#6e6e6e] hero-shadow mt-7 mb-2">My Toolkit</h2>
             <p className="text-2xl text-gray-400 font-bold">From sleek front-end builds to dynamic animations, I use modern tools like:</p>
         </div>
-        <div className='flex justify-evenly items-center mt-20'>
-            <img src={JSlogo} alt="react logo" width={70}  />
-            <img src={HTMLlogo} alt="react logo" width={60}  />
-            <img src={CSSlogo} alt="react logo" width={60}  />
-            <img src={ReactLogo} alt="react logo" width={70}  />
-            <img src={TailwindLogo} alt="react logo" width={70}  />
-            <img src={GSAPlogo} alt="react logo" width={60}  />
-            <img src={FramerMotionLogo} alt="react logo" width={60}  />
-            <img src={GitLogo} alt="react logo" width={60}  />
-            <img src={FigmaLogo} alt="react logo" width={40} />
+        <div className='flex justify-center items-center space-x-30 mt-20 logo-wrapper'>
+            <div className='flex flex-row justify-center items-center space-x-15 md:space-x-25 logo-list'>
+                <div className='logo-item'>
+                    <img src={JSlogo} alt="react logo" width={60}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={HTMLlogo} alt="react logo" width={55}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={CSSlogo} alt="react logo" width={55}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={ReactLogo} alt="react logo" width={70}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={TailwindLogo} alt="tailwind logo" width={70}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={GSAPlogo} alt="gsap logo" width={60}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={FramerMotionLogo} alt="framer motion logo" width={60}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={GitLogo} alt="git logo" width={60}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={FigmaLogo} alt="figma logo" width={40} />
+                </div>
+                <div className='logo-item'>
+                    <img src={JSlogo} alt="react logo" width={60}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={HTMLlogo} alt="react logo" width={55}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={CSSlogo} alt="react logo" width={55}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={ReactLogo} alt="react logo" width={70}/>
+                </div>
+                <div className='logo-item'>
+                    <img src={TailwindLogo} alt="tailwind logo" width={70}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={GSAPlogo} alt="gsap logo" width={60}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={FramerMotionLogo} alt="framer motion logo" width={60}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={GitLogo} alt="git logo" width={60}  />
+                </div>
+                <div className='logo-item'>
+                    <img src={FigmaLogo} alt="figma logo" width={40} />
+                </div>
+            </div>
         </div>
         <div>
             <p className="text-2xl text-gray-400 font-bold mt-20">Every project is built for performance, beauty, and scalability.</p>
